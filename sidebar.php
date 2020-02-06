936bee31e5bc8a6dbbabe932a0d966b97d72a054
apathetic <wes.hatch@gmail.com>
2020-02-05 23:16:03 -0500

    bringing some touch of modernity back

--- sidebar.php
+++ sidebar.php
@@ -1,39 +1,41 @@
-<?php /* NOTE: change depth and un/toggle commented out menu code  */ ?>
+
 	<div id="sidebar">
 		<div id="toggle"></div>
+		<div id="scroller">
+			<nav>
+				<ul>
+					<?php wp_list_pages( array(
+						'depth' => 2,
+						'sort_column' => 'menu_order',
+						'title_li' => ''
+					)); ?>
+				</ul>
+			</nav>
 
-		<nav>
-			<ul>
-				<?php wp_list_pages( array(
-					'depth' => 2,
-					'sort_column' => 'menu_order',
-					// 'sort_column' => 'post_date',
-					// 'sort_order' => 'DESC',
-					'title_li' => ''
-				)); ?>
+			<ul class="address clearfix">
+				<li><a class="icon-facebook" href="http://www.facebook.com/dil.hildebrand.35" target="_blank"></a></li>
+				<li><a class="icon-envelope" href="mailto:d&#105;&#108;h&#105;&#108;debr&#97;nd&#64;out&#108;ook.com"></a></li>
+				<li><a class="icon-instagram" href="https://www.instagram.com/dil_hildebrand/"></a></li>
 			</ul>
 
-			<?php /* * /
-				if($post->post_parent)
-					$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-				else
-					$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
 
-				if ($children) { ?>
-					<ul class="children">
-						<?php echo $children; ?>
-					</ul>
-			<?php } /* */ ?>
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+				<form action="//dilhildebrand.us7.list-manage.com/subscribe/post?u=faeca561599aaeaa6105431ca&amp;id=a279f43d28" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+						<label for="mce-EMAIL">Exhibition notifications</label>
+						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_faeca561599aaeaa6105431ca_a279f43d28" tabindex="-1" value=""></div>
+						<div class="clear"><input type="submit" value="subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					</div>
+				</form>
+			</div>
+			<!--End mc_embed_signup-->
 
-		</nav>
+			<!--
+			<p class="copy">&copy; <?php echo date("Y"); ?></p>
+			-->
 
-		<ul class="address clearfix">
-			<li><a class="icon-facebook" href="http://www.facebook.com/dil.hildebrand.35" target="_blank"></a><li>
-			<li><a class="icon-envelope" href="mailto:d&#105;&#108;h&#105;&#108;debr&#97;nd&#64;out&#108;ook.com"></a><li>
-			<li><a class="icon-pinterest" href="https://www.pinterest.com/dilhildebrand99/dil-hildebrand/"></a><li>
-			<li><a class="icon-twitter" href="https://twitter.com/dilhildebrand"></a><li>
-		</ul>
-
-		<p class="copy">&copy; <?php echo date("Y"); ?></p>
-
+		</div>
 	</div>
